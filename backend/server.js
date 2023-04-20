4d8733985bc7f83b8f7ee614a9b27e61d0b8ecbc
Jatin <75074656+jatin1322@users.noreply.github.com>
2023-04-20 13:20:52 +0530

    2/7 Completed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express= require('express');
 const app=express();
-// home route
+
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://Jatin:admin@cluster0.n3btd0y.mongodb.net/?retryWrites=true&w=majority").then(function(){
+
 app.get("/",function(req,res){
     res.send("This is a home back");
 
@@ -12,6 +15,10 @@ app.get("/notes",function(req,res){
     res.send("This is a Notes Page");
 
 });
+
+});
+// home route
+
 
 app.listen(5000,function(){
     console.log("Server Started");
