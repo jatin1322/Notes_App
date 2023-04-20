629ddc91001272c34813931799722128fb804182
Jatin <75074656+jatin1322@users.noreply.github.com>
2023-04-21 00:03:56 +0530

    Final touch

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -11,21 +11,21 @@ class ApiService {
     Uri requestUri = Uri.parse("$_baseUrl/add");
     var response = await http.post(requestUri, body: note.toMap());
     var decoded = jsonDecode(response.body);
-    log(decoded.toString());
+   // log(decoded.toString());
   }
 
   static Future<void> deleteNote(Note note) async {
     Uri requestUri = Uri.parse("$_baseUrl/delete");
     var response = await http.post(requestUri, body: note.toMap());
     var decoded = jsonDecode(response.body);
-    log(decoded.toString());
+    //log(decoded.toString());
   }
 
   static Future<List<Note>> fetchNotes(String userid) async {
     Uri requestUri = Uri.parse("$_baseUrl/list");
     var response = await http.post(requestUri, body: {"userid": userid});
     var decoded = jsonDecode(response.body);
-    log(decoded.toString());
+    //log(decoded.toString());
     List<Note> notes = [];
     for (var noteMap in decoded) {
       Note newNote = Note.fromMap(noteMap);
