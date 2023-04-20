9b9cd95ef9365f4a8725810453b63ef1a58da6a3
Jatin <75074656+jatin1322@users.noreply.github.com>
2023-04-20 11:46:18 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app=express();
+// home route
+app.get("/",function(req,res){
+    res.send("This is a home back");
+
+});
+
+// notes route
+
+app.get("/notes",function(req,res){
+    res.send("This is a Notes Page");
+
+});
+
+app.listen(5000);
